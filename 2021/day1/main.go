14bbda42a597be01d187e24d169a32320bb469cb
Purushotham <purushotham@hasura.io>
2021-12-07 23:56:30 +0530

    add 2021-1 p2

--- 2021/day1/main.go
+++ 2021/day1/main.go
@@ -8,22 +8,13 @@ import (
 	"strconv"
 )
 
-func main() {
-
-	if len(os.Args) != 2 {
-		log.Fatal("It should have only 2 arguments including main.go")
-	}
-	inputFile := os.Args[1]
+func IncreasingCount(scanner *bufio.Scanner) int64 {
 	var ans = int64(0)
-	inputBytes, err := os.Open(inputFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	scanner := bufio.NewScanner(inputBytes)
-
 	var inputString string
 	var inputNumber int
+	var err error
 	var prevNumber int = -1
+
 	for scanner.Scan() {
 		inputString = scanner.Text()
 		inputNumber, err = strconv.Atoi(inputString)
@@ -39,6 +30,55 @@ func main() {
 		}
 		prevNumber = inputNumber
 	}
-	fmt.Println(ans)
 
+	return ans
+}
+
+func IncreasingCountInWindow(scanner *bufio.Scanner) int64 {
+	var ans = int64(0)
+	var inputString string
+	inputNumbers := []int{}
+	var inputNumber int
+	var err error
+
+	for scanner.Scan() {
+		inputString = scanner.Text()
+		inputNumber, err = strconv.Atoi(inputString)
+		if err != nil {
+			log.Fatal(err)
+		}
+		inputNumbers = append(inputNumbers, inputNumber)
+	}
+
+	if len(inputNumbers) <= 3 {
+		log.Fatal("Given only 3 numbers so we can't compare the windows as only 1 is formed")
+	}
+
+	for i := 3; i < len(inputNumbers); i++ {
+		if inputNumbers[i] > inputNumbers[i-3] {
+			ans++
+		}
+	}
+
+	return ans
+}
+
+func main() {
+
+	if len(os.Args) != 2 {
+		log.Fatal("It should have only 2 arguments including main.go")
+	}
+	inputFile := os.Args[1]
+	inputBytes, err := os.Open(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(inputBytes)
+	fmt.Println(IncreasingCount(scanner))
+	inputBytes, err = os.Open(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner = bufio.NewScanner(inputBytes)
+	fmt.Println(IncreasingCountInWindow(scanner))
 }
